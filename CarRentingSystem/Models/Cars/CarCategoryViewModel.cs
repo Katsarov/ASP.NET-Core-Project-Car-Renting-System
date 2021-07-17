11a1cb1cebf3b8070fe950be2c4b9f5113c1e6bf
Kalin Katsarov <k.l.katsarov@gmail.com>
2021-07-17 16:34:31 +0300

    add

---
+++ CarRentingSystem/Models/Cars/CarCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace CarRentingSystem.Models.Cars
+{
+    public class CarCategoryViewModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+    }
+}
