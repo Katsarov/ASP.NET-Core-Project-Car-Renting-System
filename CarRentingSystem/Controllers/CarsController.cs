dcc336192c244a59c9240046b09aca5b8f265768
Kalin Katsarov <k.l.katsarov@gmail.com>
2021-08-01 11:17:14 +0300

    Implementing car searching, paging and sorting

--- CarRentingSystem/Controllers/CarsController.cs
+++ CarRentingSystem/Controllers/CarsController.cs
@@ -41,7 +41,7 @@ namespace CarRentingSystem.Controllers
                 CarSorting.DateCreated or _ => carsQuery.OrderByDescending(c => c.Id)
             };
 
-            var totalCars = this.data.Cars.Count();
+            var totalCars = carsQuery.Count();
 
             var cars = carsQuery 
                 .Skip((query.CurrentPage - 1) * AllCarsQueryModel.CarsPerPage)
